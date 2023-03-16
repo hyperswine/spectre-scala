2763bfc7d177024229808a6f7422393ef01053f5
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2023-03-17 07:13:43 +1100

    Ok

--- spectre/src/DecoupledGCD.scala
+++ spectre/src/DecoupledGCD.scala
@@ -9,25 +9,25 @@ class GcdInputBundle(val w: Int) extends Bundle {
 class GcdOutputBundle(val w: Int) extends Bundle {
   val value1 = UInt(w.W)
   val value2 = UInt(w.W)
-  val gcd    = UInt(w.W)
+  val gcd = UInt(w.W)
 }
 
 class DecoupledGcd(width: Int) extends Module {
   val input = IO(Flipped(Decoupled(new GcdInputBundle(width))))
   val output = IO(Decoupled(new GcdOutputBundle(width)))
 
-  val xInitial    = Reg(UInt())
-  val yInitial    = Reg(UInt())
-  val x           = Reg(UInt())
-  val y           = Reg(UInt())
-  val busy        = RegInit(false.B)
+  val xInitial = Reg(UInt())
+  val yInitial = Reg(UInt())
+  val x = Reg(UInt())
+  val y = Reg(UInt())
+  val busy = RegInit(false.B)
   val resultValid = RegInit(false.B)
 
-  input.ready := ! busy
+  input.ready := !busy
   output.valid := resultValid
   output.bits := DontCare
 
-  when(busy)  {
+  when(busy) {
     when(x > y) {
       x := x - y
     }.otherwise {
