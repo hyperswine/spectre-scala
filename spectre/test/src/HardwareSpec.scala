cf54bc39e2671b5e77359e7ba1a19885ba35ae3b
Jasen Qin <jiasheng.qin@student.unsw.edu.au>
2023-03-17 14:01:27 +1100

    We;;

--- spectre/test/src/HardwareSpec.scala
+++ spectre/test/src/HardwareSpec.scala
@@ -1,4 +1,3 @@
-
 import chisel3._
 import chiseltest._
 import chisel3.experimental.BundleLiterals._
@@ -12,6 +11,30 @@ object HardwareSpec extends ChiselUtestTester {
         hw.io.mem_input.poke(1.asUInt)
         hw.clock.step(1)
         hw.io.mem_output.expect(1)
+      }
+    }
+    test("Vector") {
+      testCircuit(new VectorUnit) { vec_unit =>
+        def testOperation(
+          op:             UInt,
+          dataType:       UInt,
+          src1:           BigInt,
+          src2:           BigInt,
+          src3:           BigInt,
+          expectedResult: BigInt
+        ): Unit = {
+          vec_unit.io.op.poke(dataType)
+          vec_unit.io.dataType.poke(dataType)
+          vec_unit.io.src1.poke(src1)
+          vec_unit.io.src2.poke(src2)
+          vec_unit.io.src2.poke(src2)
+
+          vec_unit.clock.step(1)
+          vec_unit.io.result.expect(expectedResult)
+        }
+
+        // Example: Add two 8-bit integers
+        testOperation(0.U, 0.U, 1, 2, 0, 3)
       }
     }
   }
