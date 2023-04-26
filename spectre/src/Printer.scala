76e9ddbbb59a6f4fcba99529db022cccc459d7a6
Jasen Qin <jiasheng.qin@student.unsw.edu.au>
2023-04-26 23:52:12 +1000

    ADD

---
+++ spectre/src/Printer.scala
@@ -0,0 +1,7 @@
+// something about klipper
+// and exposing the core functionalities to the network interface
+// maybe even have a config DEFAULT that implements a TCP method of comms
+
+// slicer though... maybe a default "just works" set, and some other software on top of that to detect if its just not going to work
+
+
