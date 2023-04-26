76b779e22b04bcb012e79ca412267a540dcbdd1b
Jasen Qin <jiasheng.qin@student.unsw.edu.au>
2023-04-27 00:05:07 +1000

    AGG

--- spectre/src/Printer.scala
+++ spectre/src/Printer.scala
@@ -4,4 +4,14 @@
 
 // slicer though... maybe a default "just works" set, and some other software on top of that to detect if its just not going to work
 
+import chisel3._
+import circt.stage.ChiselStage
 
+class Printer extends Module {
+  val io = IO(new Bundle {})
+  printf("hello world\n")
+}
+
+object VerilogMain extends App {
+  ChiselStage.emitSystemVerilog(new Printer)
+}
