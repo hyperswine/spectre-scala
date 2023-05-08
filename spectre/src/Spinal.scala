65b45a00449a9ebab06b3f7acea6e9a2c73119e2
Jasen Qin <jiasheng.qin@student.unsw.edu.au>
2023-05-09 05:40:17 +1000

    Add2

--- spectre/src/Spinal.scala
+++ spectre/src/Spinal.scala
@@ -1,23 +1,39 @@
 import spinal.core._
 
-class AND_Gate extends Component {
-
-  /**
-    * This is the component definition that corresponds to
-    * the VHDL entity of the component
-    */
+class Playground extends Component {
   val io = new Bundle {
     val a = in.Bool()
     val b = in.Bool()
     val c = out.Bool()
+    val readData = out.Bits()
+    val writeValid = in.Bool()
+    val readValid = in.Bool()
+
+    val writeAddress = in.UInt()
+    val writeData = in.Bits()
+    val readAddress = in.UInt()
   }
+
+  val cond = Bool()
+  val mem = Mem(Bits(32 bits), wordCount = 256)
+
+  mem.write(
+    enable  = io.writeValid,
+    address = io.writeAddress,
+    data    = io.writeData
+  )
+
+  io.readData := mem.readSync(
+    enable  = io.readValid,
+    address = io.readAddress
+  )
 
   // Here we define some asynchronous logic
   io.c := io.a & io.b
 }
 
-object AND_Gate {
+object Playground {
   // Let's go
   def main(args: Array[String]): Unit =
-    SpinalVhdl(new AND_Gate)
+    SpinalVhdl(new Playground)
 }
--- spectre/test/src/HardwareSpec.scala
+++ spectre/test/src/HardwareSpec.scala
@@ -6,36 +6,36 @@ import utest._
 
 object HardwareSpec extends ChiselUtestTester {
   val tests = Tests {
-    test("Hardware") {
-      testCircuit(new Hardware) { hw =>
-        hw.io.mem_input.poke(1.asUInt)
-        hw.clock.step(1)
-        hw.io.mem_output.expect(1)
-      }
-    }
-    test("Vector") {
-      testCircuit(new VectorUnit) { vec_unit =>
-        def testOperation(
-          op:             UInt,
-          dataType:       UInt,
-          src1:           BigInt,
-          src2:           BigInt,
-          src3:           BigInt,
-          expectedResult: BigInt
-        ): Unit = {
-          vec_unit.io.op.poke(dataType)
-          vec_unit.io.dataType.poke(dataType)
-          vec_unit.io.src1.poke(src1)
-          vec_unit.io.src2.poke(src2)
-          vec_unit.io.src2.poke(src2)
+    // test("Hardware") {
+    //   testCircuit(new Hardware) { hw =>
+    //     hw.io.mem_input.poke(1.asUInt)
+    //     hw.clock.step(1)
+    //     hw.io.mem_output.expect(1)
+    //   }
+    // }
+    // test("Vector") {
+    //   testCircuit(new VectorUnit) { vec_unit =>
+    //     def testOperation(
+    //       op:             UInt,
+    //       dataType:       UInt,
+    //       src1:           BigInt,
+    //       src2:           BigInt,
+    //       src3:           BigInt,
+    //       expectedResult: BigInt
+    //     ): Unit = {
+    //       vec_unit.io.op.poke(dataType)
+    //       vec_unit.io.dataType.poke(dataType)
+    //       vec_unit.io.src1.poke(src1)
+    //       vec_unit.io.src2.poke(src2)
+    //       vec_unit.io.src2.poke(src2)
 
-          vec_unit.clock.step(1)
-          vec_unit.io.result.expect(expectedResult)
-        }
+    //       vec_unit.clock.step(1)
+    //       vec_unit.io.result.expect(expectedResult)
+    //     }
 
-        // Example: Add two 8-bit integers
-        testOperation(0.U, 8.U, 1, 2, 0, 3)
-      }
-    }
+    //     // Example: Add two 8-bit integers
+    //     testOperation(0.U, 8.U, 1, 2, 0, 3)
+    //   }
+    // }
   }
 }
